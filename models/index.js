62a918377cf6efb2ede8d3ca7d84fac857fc1e52
Dan <bmdmartinez@gmail.com>
2021-08-10 11:48:20 -0400

    created all routes for creating posts

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// defining the relationship of post to the user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 // exports an object with the user object as a propery
 module.exports = { User, Post };
